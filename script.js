ad40a0927df6eeb17d62fa3f2fc552a7702a0947
Ka Cheng Ieong <ieongkacheng@gmail.com>
2020-06-01 23:46:06 -0400

    remove rows

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-
+const button = document.getElementById("removeRow");
+const table = document.getElementById("table");
+const row = table.getElementsByTagName("tr");
+button.addEventListener("click", function(e){
+    if (table.getElementsByTagName("tr").length === 0) {
+        alert("The grid have no rows");
+    }
+    else {
+        row[length - 1].remove();
+    }
+});
  
 
 
