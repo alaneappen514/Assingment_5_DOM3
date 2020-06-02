f5b0bea40869a4504b0c1611f6a868f7d8d01726
Alan Eappen <eappenalan123@gmail.com>
2020-06-02 00:02:14 -0400

    Added coloring

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let count = 0;
+let colorchoice;
 
 
 document.getElementById("addRow").addEventListener("click", function(event){
@@ -7,11 +8,15 @@ document.getElementById("addRow").addEventListener("click", function(event){
     let createRow =  document.createElement('tr');
     if(count === 0)
     {
-    tablebody.appendChild(createRow);
-    createRow.appendChild(document.createElement('th'));
+    
+     tablebody.appendChild(createRow).setAttribute("class", "Row");
+     createRow.classList.add(colorchoice)
+     createRow.appendChild(document.createElement('th'));
+     
     }
     else{
-        tablebody.appendChild(createRow);
+        tablebody.appendChild(createRow).setAttribute("class", "Row");
+        createRow.classList.add(colorchoice)
         for(i = 0; i< count; i++)
         {
             addColumns(createRow);
@@ -20,9 +25,31 @@ document.getElementById("addRow").addEventListener("click", function(event){
 
     });
 
-document.getElementById("addColumn").addEventListener("click", function(event){
+document.getElementById("addColumn").addEventListener("click", function(){
      count++;
  });
+
+ document.getElementById("addColor").addEventListener("click",function(){
+    var e = document.getElementById("color");
+    colorchoice = e.options[e.selectedIndex].text;
+ })
+
+ document.getElementById("addAllColor").addEventListener("click", function(event){
+    
+    elements = document.getElementsByClassName("Row");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.add(colorchoice);
+    }
+});
+
+document.getElementById("clearAllColor").addEventListener("click", function(event){
+    
+    elements = document.getElementsByClassName("Row");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove(colorchoice);
+    }
+});
+ 
 
  function addColumns(element){
     element.appendChild(document.createElement('th'));
