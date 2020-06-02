17273d594da8b23be1b27db4608563639760f9b9
Ka Cheng Ieong <ieongkacheng@gmail.com>
2020-06-01 22:30:51 -0400

    Delete code

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
 
-let count = 0;
-
-
-document.getElementById("addRow").addEventListener("click", function(event){
-    let tablebody = document.getElementById("table");
-    let createRow =  document.createElement('tr');
-    if(count === 0)
-    {
-    tablebody.appendChild(createRow);
-    createRow.appendChild(document.createElement('th'));
-    }
-    else{
-        tablebody.appendChild(createRow);
-        for(i = 0; i< count; i++)
-        {
-            addColumns(createRow);
-        }
-    }
-
-    });
-
-document.getElementById("addColumn").addEventListener("click", function(event){
-     count++;
- });
-
- function addColumns(element){
-    element.appendChild(document.createElement('th'));
- }
  
 
 
